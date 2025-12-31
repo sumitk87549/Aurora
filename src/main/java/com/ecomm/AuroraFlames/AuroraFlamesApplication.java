6980a21de0f222f69a11676d55ebbacdf6269470
sumitk87549 <sumitk87549@gmail.com>
2025-12-31 11:55:27 +0530

    init

---
+++ src/main/java/com/ecomm/AuroraFlames/AuroraFlamesApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.AuroraFlames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraFlamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraFlamesApplication.class, args);
+	}
+
+}
