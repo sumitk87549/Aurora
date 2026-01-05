09c996dd6fa29e51ace710366ae8ba3e9c84f892
sumitk87549 <sumitk87549@gmail.com>
2026-01-05 06:45:26 +0530

    angular removed

--- client/src/app/config/api.config.ts
+++
@@ -1,4 +0,0 @@
-// In development (ng serve), the proxy.conf.json forwards '/api' to 'http://localhost:8081/api'
-// In production (bundled), the app is served from the same origin, so '/api' works natively.
-
-export const API_URL = '/api';
